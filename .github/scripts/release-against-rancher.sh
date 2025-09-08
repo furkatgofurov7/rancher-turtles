a6980ad9a7bffccd8a7e6da1a601dfcf6d6a8328
Furkat Gofurov <furkat.gofurov@suse.com>
2025-09-09 00:06:52 +0300

    Update release-against-rancher.sh
    Signed-off-by: Furkat Gofurov <furkat.gofurov@suse.com>

--- .github/scripts/release-against-rancher.sh
+++ .github/scripts/release-against-rancher.sh
@@ -1,37 +1,3 @@
-# #!/bin/bash
-# #
-# # Submit new Turtles version against rancher/rancher
-
-# set -ue
-
-# NEW_TURTLES_VERSION="$1"    # e.g. 0.23.0-rc.0
-# NEW_CHART_VERSION="$2"    # e.g. 101.1.0
-
-# RANCHER_DIR=${RANCHER_DIR-"$(dirname -- "$0")/../../../rancher"}
-
-# pushd "${RANCHER_DIR}" > /dev/null
-
-# # Check if version is available online
-# CHART_DEFAULT_BRANCH=$(grep "ARG CHART_DEFAULT_BRANCH=" package/Dockerfile | cut -d'=' -f2)
-# if ! curl -s --head --fail "https://github.com/furkatgofurov7/charts/raw/${CHART_DEFAULT_BRANCH}/assets/turtles/turtles-${NEW_CHART_VERSION}+up${NEW_TURTLES_VERSION}.tgz" > /dev/null; then
-#     echo "Version ${NEW_CHART_VERSION}+up${NEW_TURTLES_VERSION} does not exist in the branch ${CHART_DEFAULT_BRANCH} in furkatgofurov7/charts"
-#     exit 1
-# fi
-
-# if [ -e build.yaml ]; then
-#     sed -i -e "s/turtlesVersion: .*$/turtlesVersion: ${NEW_CHART_VERSION}+up${NEW_TURTLES_VERSION}/" build.yaml
-#     go generate
-#     git add build.yaml pkg/buildconfig/constants.go
-# else
-#     sed -i -e "s/ENV CATTLE_RANCHER_TURTLES_VERSION=.*$/ENV CATTLE_RANCHER_TURTLES_VERSION=${NEW_CHART_VERSION}+up${NEW_TURTLES_VERSION}/" package/Dockerfile
-#     git add package/Dockerfile
-# fi
-
-# git commit -m "Updating to Turtles v${NEW_TURTLES_VERSION}"
-
-# popd > /dev/null
-
-
 #!/bin/bash
 #
 # Bumps turtles version in a locally checked out rancher/rancher repository
@@ -40,10 +6,10 @@
 #   ./release-against-rancher.sh <path to rancher repo> <new turtles release>
 #
 # Example:
-# ./release-against-charts.sh "${GITHUB_WORKSPACE}" "v0.5.0-rc.14"
+# ./release-against-charts.sh "${GITHUB_WORKSPACE}" "v0.23.0-rc.0"
 
 RANCHER_DIR=$1
-NEW_TURTLES_VERSION=$2   # e.g. v0.5.2-rc.3
+NEW_TURTLES_VERSION=$2   # e.g. v0.23.0-rc.0
 
 usage() {
     cat <<EOF
@@ -83,7 +49,7 @@ set -ue
 
 pushd "${RANCHER_DIR}" > /dev/null
 
-# Get the turtles version (eg: 0.5.0-rc.12)
+# Get the turtles version (eg: 0.23.0-rc.0)
 if ! PREV_TURTLES_VERSION_SHORT=$(yq -r '.turtlesVersion' ./build.yaml | sed 's|.*+up||'); then
     echo "Unable to get turtles version from ./build.yaml. The content of the file is:"
     cat ./build.yaml
